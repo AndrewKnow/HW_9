58b07467262268d39cc9d325946fe40379d2eaba
AndrewKnow <92088740+AndrewKnow@users.noreply.github.com>
2022-10-05 16:40:24 +0300

    Добавьте файлы проекта.

---
+++ ДЗ_9/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ДЗ_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ImageDownloader imageDownloader = new ImageDownloader();
+
+            imageDownloader.Begin += ImageDownloader_Begin;
+            imageDownloader.Finish += ImageDownloader_Finish;
+
+            // imageDownloader.Download();
+            var result = imageDownloader.DownloadAsync();
+
+            // Console.WriteLine("Нажмите любую клавишу для выхода");
+
+            Console.WriteLine(@"Нажмите клавишу ""A"" (русская, верх. регистр) для выхода или любую другую клавишу для проверки статуса скачивания");
+
+            // Console.ReadLine();
+
+            while (true)
+            {
+
+                var key = Console.ReadKey();
+                switch (key.KeyChar)
+                {
+                    case 'А':
+                        return;
+                    default:
+                        
+
+                        if (result.IsCompleted == true)
+                        {
+                            Console.WriteLine(result.IsCompleted.ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine(result.IsCompleted.ToString());
+                        }
+
+                        break;
+
+                }
+            }
+
+        }
+
+        private static void ImageDownloader_Begin()
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Скачивание файла началось");
+            Console.WriteLine("--------------------------");
+        }
+
+        private static void ImageDownloader_Finish()
+        {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Скачивание файла закончилось");
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
